e7e0cd3dc74eab47672727346b06d50a3f23f2d0
Fred452 <fredyarce452gmail.com>
2023-02-15 22:42:03 -0300

    FlewBox y Overflow

---
+++ 05-Dispociciones/js/index.js
@@ -0,0 +1,8 @@
+const num = document.querySelectorAll("#cal")
+const pan = document.querySelector(".pantalla")
+
+num.forEach(element => {
+    element.addEventListener("click", event => {
+        // pan.style.color = "red"
+    })
+});
